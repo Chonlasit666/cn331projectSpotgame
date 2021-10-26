106449233c141bea8ec3082fbd8c2147042216c8
raned.chu <6210612864@student.tu.ac.th>
2021-10-26 21:21:00 +0700

    minor version of spotify music quiz have a new feature (customUser) this feature change how to login to admin and all about login PS.it's still a demo I have to test it and check it function again ty

--- spotgame/settings.py
+++ spotgame/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'aboutme',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.CustomUser'
