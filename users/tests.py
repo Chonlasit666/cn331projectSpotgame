07c9aef153689add75534c8825e8f036d270beae
raned.chu <6210612864@student.tu.ac.th>
2021-10-27 18:09:44 +0700

    change some test PS.there're two models that haven't write a test yet

--- users/tests.py
+++ users/tests.py
@@ -7,34 +7,34 @@ class UsersManagersTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
-            email='normal@user.com', password='foo')
+            email='normal@user.com', username='normal', password='foo')
         self.assertEqual(user.email, 'normal@user.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
         try:
-            self.assertIsNone(user.username)
+            self.assertIsNotNone(user.username)
         except AttributeError:
             pass
         with self.assertRaises(TypeError):
             User.objects.create_user()
         with self.assertRaises(TypeError):
-            User.objects.create_user(email='')
+            User.objects.create_user(email='', username='')
         with self.assertRaises(ValueError):
-            User.objects.create_user(email='', password="foo")
+            User.objects.create_user(email='', username='', password="foo")
 
     def test_create_superuser(self):
         User = get_user_model()
         admin_user = User.objects.create_superuser(
-            email='super@user.com', password='foo')
+            email='super@user.com', username='super', password='foo')
         self.assertEqual(admin_user.email, 'super@user.com')
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
         try:
-            self.assertIsNone(admin_user.username)
+            self.assertIsNotNone(admin_user.username)
         except AttributeError:
             pass
         with self.assertRaises(ValueError):
             User.objects.create_superuser(
-                email='super@user.com', password='foo', is_superuser=False)
+                email='super@user.com', username='super', password='foo', is_superuser=False)
