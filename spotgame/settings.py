f3e69ea5ad8c1828b433f0d405100f7aa517e535
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-10-30 05:35:17 +0700

    add chat room

--- spotgame/settings.py
+++ spotgame/settings.py
@@ -147,5 +147,14 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
-
+'''
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+        },
+    },
+}
+'''
 AUTH_USER_MODEL = 'users.CustomUser'
