00bffe831ad27f87a2082d502a2611191a3c162d
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-10-30 06:06:35 +0700

    pls

--- spotgame/settings.py
+++ spotgame/settings.py
@@ -125,15 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
