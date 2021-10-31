3570c12fc6a5a6fa7978561b3a1fa92a510835d1
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-10-31 12:06:28 +0700

    add link everypage to heroku

--- spotifymusicgame/urls.py
+++ spotifymusicgame/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:room_name>/', views.room, name='room'),
+    path('spotifymusicgame/<int:room_name>/', views.room, name='room'),
     path('aboutme/',views.about, name='aboutme'),
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view , name = 'logout'),
