6062d30e31e5c9f4638638bcb9135100f63a25b3
raned.chu <6210612864@student.tu.ac.th>
2021-10-31 15:56:18 +0700

    add more models

--- spotifymusicgame/models.py
+++ spotifymusicgame/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class playList(models.Model):
+    url = models.URLField(max_length=255, blank=False, null=False)
+    count = models.IntegerField(default=0)
+
+
+class roomInfo(models.Model):
+    player = models.ManyToManyField(
+        settings.AUTH_USER_MODEL)
+    url = models.ForeignKey(playList, on_delete=models.CASCADE)
+    max_player = models.IntegerField(default=8)
+    max_song = models.IntegerField(default=10)
+    guess_time = models.IntegerField(default=15)
+
+
+class played(models.Model):
+    played = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+    room = models.ForeignKey(roomInfo, on_delete=models.CASCADE)
+    max_score = models.IntegerField(default=10)
