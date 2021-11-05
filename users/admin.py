14a8ad3c780f474da37017e907b8afa042dd6ee3
6210612823 <60337806+6210612823@users.noreply.github.com>
2021-11-05 20:56:07 +0700

    New Sign Up Page

--- users/admin.py
+++ users/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .forms import UserCreationForm, CustomUserChangeForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser, FriendList, FriendRequest
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = UserCreationForm
+    add_form =CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ('email', 'is_staff', 'is_active',)
