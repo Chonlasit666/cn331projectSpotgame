9508bbd12e723d0365c04fd5684c1bf296d94b99
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-06 15:42:00 +0700

    maybe db

--- spotgame/settings.py
+++ spotgame/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,13 +82,24 @@ WSGI_APPLICATION = 'spotgame.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'github-actions',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
+        }
+    }
 
 
 # Password validation
@@ -164,5 +176,5 @@ CHANNEL_LAYERS = {
     },
 }
 
-
+django_heroku.settings(locals())
 AUTH_USER_MODEL = 'users.CustomUser'
