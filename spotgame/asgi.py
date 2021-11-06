9662aeea13072d22d94227185b11a596a123eb81
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-06 17:04:33 +0700

    test

--- spotgame/asgi.py
+++ spotgame/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -15,7 +16,7 @@ from django.core.asgi import get_asgi_application
 import spotifymusicgame.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotgame.settings')
-
+django.setup()
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
