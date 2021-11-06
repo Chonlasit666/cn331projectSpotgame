11579b292e5beda6c31f32623062efe18ba150dc
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-06 15:52:06 +0700

    try

--- spotgame/settings.py
+++ spotgame/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -82,24 +81,13 @@ WSGI_APPLICATION = 'spotgame.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'github-actions',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432'
-        }
-    }
 
 
 # Password validation
@@ -157,7 +145,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ASGI_APPLICATION = "spotgame.asgi.application"
-'''
+
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
@@ -175,6 +163,6 @@ CHANNEL_LAYERS = {
         },
     },
 }
+'''
 
-django_heroku.settings(locals())
 AUTH_USER_MODEL = 'users.CustomUser'
