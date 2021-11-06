a91907db51b0b05b2e85aad3cc6629627a535a88
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-06 10:41:11 +0700

    test deploy

--- spotgame/settings.py
+++ spotgame/settings.py
@@ -138,12 +138,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+'''
+ASGI_APPLICATION = "spotgame.asgi.application"
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    },
+}
+'''
 ASGI_APPLICATION = "spotgame.routing.application"
 
 CHANNEL_LAYERS = {
