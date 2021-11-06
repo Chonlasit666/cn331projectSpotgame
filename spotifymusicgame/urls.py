b69ed17e98ea6e90aa5f85ba5a4719c662d47b15
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-06 11:01:38 +0700

    legacy v.1.0.1

--- spotifymusicgame/urls.py
+++ spotifymusicgame/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-app_name = "smg"
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('(?P<room_name>\d+)/$', views.room, name='room'),
-    path('aboutme/', views.about, name='aboutme'),
-    path('createroom', views.create_room_view, name='createroom'),
+    path('spotifymusicgame/<int:room_name>/', views.room, name='room'),
+    path('aboutme/',views.about, name='aboutme'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view , name = 'logout'),
 ]
--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 
 from .managers import CustomUserManager
 
+
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=16)
     email = models.EmailField(_('email address'), unique=True)
