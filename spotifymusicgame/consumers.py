3555480683d29cd46953121e5a028be873642ae8
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-06 16:25:18 +0700

    maybe db

--- spotifymusicgame/consumers.py
+++ spotifymusicgame/consumers.py
@@ -1,7 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 #from channels.db import database_sync_to_async
-from .models import *
+#from .models import *
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
