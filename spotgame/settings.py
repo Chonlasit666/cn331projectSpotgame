afcea17741063b4c29b1f5e06ad172386da5afa4
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-06 13:26:51 +0700

    test app

--- spotgame/settings.py
+++ spotgame/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_extensions',
     'channels',
-    'spotifymusicgame.apps.UsersConfig',
+    'spotifymusicgame.apps.SpotifymusicgameConfig',
     'users',
 
 ]
