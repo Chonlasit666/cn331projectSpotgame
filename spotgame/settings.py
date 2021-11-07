3b58eeadd07c29d1dbce03e3bb278f345cace23c
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-07 07:59:18 +0700

    test

--- spotgame/settings.py
+++ spotgame/settings.py
@@ -143,9 +143,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-ASGI_APPLICATION = "spotgame.asgi.application"
 '''
+ASGI_APPLICATION = "spotgame.asgi.application"
+
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
