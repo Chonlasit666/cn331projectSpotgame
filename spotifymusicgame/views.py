c01772c3703c01b7929756abe03bbf6923a05d32
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-11 17:50:02 +0700

    v.1.2.0

--- spotifymusicgame/views.py
+++ spotifymusicgame/views.py
@@ -2,9 +2,11 @@ from django.http.response import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
 from django.core import serializers
+from django.contrib import messages
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from .models import *
+import random
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -31,25 +33,27 @@ def room(request, room_name):
 
         count = 0
         for i in songModel.objects.filter(playlist__url=roomInfo.objects.get(id = room_name).url):
-            songs.append(str(i.song).replace("'",""))
-            artists.append(str(i.artist).replace("'",""))
-            images.append(str(i.image).replace("'",""))
-            uris.append(str(i.uri).replace("'",""))
-
+            if not i.uri:
+                pass
+            else:
+                songs.append(str(i.song).replace("'",""))
+                artists.append(str(i.artist).replace("'",""))
+                images.append(str(i.image).replace("'",""))
+                uris.append(str(i.uri).replace("'",""))
+        
+        seed = room_name
+        random.Random(seed).shuffle(songs)
+        random.Random(seed).shuffle(artists)
+        random.Random(seed).shuffle(images)
+        random.Random(seed).shuffle(uris)
 
         song_json = json.dumps(songs)
         artist_json = json.dumps(artists)
         image_json = json.dumps(images)
         uri_json = json.dumps(uris)
 
-
-        track = sp.track('spotify:track:6IG5ZOKnUryCcsvzopK23A')
         return render(request, 'spotifymusicgame/room.html', {
             'room_name': room_name,
-            "name": track['name'],
-            "artist": track['artists'][0]['name'],
-            "pic": track['album']['images'][0]['url'],
-            "url": track['preview_url'],
             'songs' : song_json,
             'artists' : artist_json,
             'images' : image_json,
@@ -63,10 +67,16 @@ def about(request):
 
 def create_room_view(request):
     ID = None
+
     if request.method == "POST":
         try:
 
             URI_ = request.POST['URI']
+
+            if not URI_:
+                messages.info(request, 'Please enter playlist URI')
+                return render(request, 'spotifymusicgame/createroom.html')
+
             Max_player = request.POST['Max_player']
             if not playList.objects.filter(url=URI_).exists():
                 playList.objects.create(url=URI_)
