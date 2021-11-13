f0d41d3ca95247bc04f79aa7fa58583cd1e6128e
6210612856 <6210612856@student.tu.ac.th>
2021-11-13 17:53:20 +0700

    update views.py and comsumer.py delete playlist on purpurse

--- spotifymusicgame/views.py
+++ spotifymusicgame/views.py
@@ -12,32 +12,26 @@ from spotipy.oauth2 import SpotifyClientCredentials
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="ffc4c1c607de49489dc5b071b326727e",
                                                            client_secret="4fd9dfe58f914768b24a034e1da88c2b"))
 
-
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("users:login"))
     else:
         return render(request, "spotifymusicgame/index.html")
 
-
 def room(request, room_name):
-    try:
-        roomInfo.objects.get(id=room_name)
+    try: 
+        roomInfo.objects.get(id = room_name)
     except:
-        messages.info(request, 'That room is not available')
         return render(request, "spotifymusicgame/index.html")
 
+   
     current_user = roomInfo.objects.get(id=room_name).player_inroom
     max_user = roomInfo.objects.get(id=room_name).max_player
     is_playing = roomInfo.objects.get(id=room_name).is_playing
-    # check if room full
-    if(current_user >= max_user):
-        messages.info(request, 'That room is already full')
+    if(current_user >= max_user) :
         return render(request, "spotifymusicgame/index.html")
-    # check if room playing
     if(is_playing):
-        messages.info(request, 'That room is already playing')
-        return render(request, "spotifymusicgame/index.html")
+         return render(request, "spotifymusicgame/index.html")
 
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("users:login"))
@@ -48,18 +42,21 @@ def room(request, room_name):
         uris = []
         dbsong = []
 
-        for i in songModel.objects.filter(playlist__url=roomInfo.objects.get(id=room_name).url):
+        for i in songModel.objects.filter(playlist__url=roomInfo.objects.get(id = room_name).url):
             if not i.uri:
                 pass
             else:
-                songs.append(str(i.song).replace("'", ""))
-                artists.append(str(i.artist).replace("'", ""))
-                images.append(str(i.image).replace("'", ""))
-                uris.append(str(i.uri).replace("'", ""))
+                songs.append(str(i.song).replace("'","").replace("[","").replace("]","").replace('"',''))
+                artists.append(str(i.artist).replace("'","").replace("[","").replace("]","").replace('"',''))
+                images.append(str(i.image).replace("'","").replace("[","").replace("]","").replace('"',''))
+                uris.append(str(i.uri).replace("'","").replace("[","").replace("]","").replace('"',''))
+
+        if len(songs) == 0:
+            return render(request, "spotifymusicgame/index.html")
 
         for j in songModel.objects.all():
-            dbsong.append(str(j.song).replace("'", ""))
-
+            dbsong.append(str(j.song).replace("'","").replace("[","").replace("]","").replace('"',''))
+        print(songs)
         seed = room_name
         random.Random(seed).shuffle(songs)
         random.Random(seed).shuffle(artists)
@@ -74,11 +71,11 @@ def room(request, room_name):
 
         return render(request, 'spotifymusicgame/room.html', {
             'room_name': room_name,
-            'songs': song_json,
-            'artists': artist_json,
-            'images': image_json,
-            'uris': uri_json,
-            'dbsong': dbsong_json,
+            'songs' : song_json,
+            'artists' : artist_json,
+            'images' : image_json,
+            'uris' : uri_json,
+            'dbsong' : dbsong_json,
         })
 
 
@@ -93,25 +90,18 @@ def create_room_view(request):
         try:
 
             URI_ = request.POST['URI']
-            Max_player = request.POST['Max_player']
-            if ((not URI_ or not Max_player) or not Max_player.isdecimal()) or (not ("spotify:playlist:") in URI_):
-                if not Max_player:
-                    messages.info(request, 'Please enter max player')
-                elif not Max_player.isdecimal():
-                    messages.info(request, 'Max player must be number')
-                if not URI_:
-                    messages.info(request, 'Please enter playlist URI')
-                elif not ("spotify:playlist:") in URI_:
-                    messages.info(request, 'Please enter valid spotify playlist')
+
+            if not URI_:
+                messages.info(request, 'Please enter playlist URI')
                 return render(request, 'spotifymusicgame/createroom.html')
 
+            Max_player = request.POST['Max_player']
             if not playList.objects.filter(url=URI_).exists():
                 playList.objects.create(url=URI_)
-            this_playlist = playList.objects.get(url=URI_)
-            this_room = roomInfo.objects.create(
-                url=this_playlist, max_player=Max_player)
+            this_playlist = playList.objects.get(url = URI_)
+            this_room = roomInfo.objects.create(url = this_playlist , max_player = Max_player)
             room_name = this_room.id
-            return HttpResponseRedirect(reverse("smg:room", args=(room_name,)))
+            return HttpResponseRedirect(reverse("smg:room", args = (room_name,)))
 
         except:
             return render(request, 'spotifymusicgame/createroom.html')
