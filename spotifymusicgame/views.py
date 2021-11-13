b50a42bd5f135517594e34ea0d1d6571a0e1b1a1
6210612823 <60337806+6210612823@users.noreply.github.com>
2021-11-13 16:31:37 +0700

    Fix error redirect

--- spotifymusicgame/views.py
+++ spotifymusicgame/views.py
@@ -26,14 +26,20 @@ def room(request, room_name):
     except:
         return render(request, "spotifymusicgame/index.html")
 
+    if not room_name.isdecimal():
+        messages.info(request, 'Please enter valid room name')
+        return render(request, "spotifymusicgame/index.html")
+        
     current_user = roomInfo.objects.get(id=room_name).player_inroom
     max_user = roomInfo.objects.get(id=room_name).max_player
     is_playing = roomInfo.objects.get(id=room_name).is_playing
     # check if room full
     if(current_user >= max_user):
+        messages.info(request, 'That room is already full')
         return render(request, "spotifymusicgame/index.html")
     # check if room playing
     if(is_playing):
+        messages.info(request, 'That room is already playing')
         return render(request, "spotifymusicgame/index.html")
 
     if not request.user.is_authenticated:
