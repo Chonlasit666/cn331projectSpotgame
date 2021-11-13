cef02638bd4a4e5afc722d68ada32f0d96c79cfc
raned.chu <6210612864@student.tu.ac.th>
2021-11-13 17:43:51 +0700

    add Test ?

--- spotifymusicgame/tests_views.py.py
+++ spotifymusicgame/tests_views.py.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class SpotifymusicgameTestCase(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+
+    def test_unauthenticated_user_cannot_room_view(self):
+        response = self.client.get(reverse('smg:room'))
+        self.assertRedirects(response, reverse('users:login'))
+
