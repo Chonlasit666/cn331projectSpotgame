6248715e6629b51023d13db5216a6473d056d5ce
Chonlasit Mooncorn <6110613020@student.tu.ac.th>
2021-11-15 01:45:20 +0700

    v.1.3.4

--- spotifymusicgame/views.py
+++ spotifymusicgame/views.py
@@ -1,10 +1,13 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django.contrib import messages
-import json
 from .models import *
+import cutlet
+
 import random
+import json
 
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -12,8 +15,11 @@ from spotipy.oauth2 import SpotifyClientCredentials
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="ffc4c1c607de49489dc5b071b326727e",
                                                            client_secret="4fd9dfe58f914768b24a034e1da88c2b"))
 
+katsu = cutlet.Cutlet()
+t = "?"
 
 def index(request):
+
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("users:login"))
     else:
@@ -24,18 +30,13 @@ def room(request, room_name):
     try:
         roomInfo.objects.get(id=room_name)
     except:
-        messages.info(request, 'That room is not available')
         return render(request, "spotifymusicgame/index.html")
-
     current_user = roomInfo.objects.get(id=room_name).player_inroom
     max_user = roomInfo.objects.get(id=room_name).max_player
     is_playing = roomInfo.objects.get(id=room_name).is_playing
-    
     if(current_user >= max_user):
-        messages.info(request, 'That room is already full')
         return render(request, "spotifymusicgame/index.html")
     if(is_playing):
-        messages.info(request, 'That room is already playing')
         return render(request, "spotifymusicgame/index.html")
 
     if not request.user.is_authenticated:
@@ -51,8 +52,13 @@ def room(request, room_name):
             if not i.uri:
                 pass
             else:
-                songs.append(str(i.song).replace("'", "").replace(
+                o = katsu.romaji(i.song)
+                if(o.find(t) == -1):
+                    songs.append(str(o).replace("'", "").replace(
                     "[", "").replace("]", "").replace('"', ''))
+                else:
+                    songs.append(str(i.song).replace("'", "").replace("[", "").replace("]", "").replace('"', ''))
+
                 artists.append(str(i.artist).replace("'", "").replace(
                     "[", "").replace("]", "").replace('"', ''))
                 images.append(str(i.image).replace("'", "").replace(
@@ -61,10 +67,15 @@ def room(request, room_name):
                     "[", "").replace("]", "").replace('"', ''))
 
         if len(songs) == 0:
+            print(" non - test")
             return render(request, "spotifymusicgame/index.html")
 
         for j in songModel.objects.all():
-            dbsong.append(str(j.song).replace("'", "").replace(
+            o = katsu.romaji(j.song)
+            if(o.find(t) == -1):
+                dbsong.append(str(o).replace("'", "").replace("[", "").replace("]", "").replace('"', ''))
+            else:
+                dbsong.append(str(j.song).replace("'", "").replace(
                 "[", "").replace("]", "").replace('"', ''))
         #print(songs)
         seed = room_name
@@ -93,6 +104,7 @@ def about(request):
     return render(request, 'spotifymusicgame/aboutme.html')
 
 
+@login_required(login_url='users:login')
 def create_room_view(request):
     ID = None
 
@@ -100,17 +112,9 @@ def create_room_view(request):
         try:
 
             URI_ = request.POST['URI']
-            Max_player = request.POST['Max_player']
-            if ((not URI_ or not Max_player) or not Max_player.isdecimal()) or (not ("spotify:playlist:") in URI_):
-                if not Max_player:
-                    messages.info(request, 'Please enter max player')
-                elif not Max_player.isdecimal():
-                    messages.info(request, 'Max player must be number')
-                if not URI_:
-                    messages.info(request, 'Please enter playlist URI')
-                elif not ("spotify:playlist:") in URI_:
-                    messages.info(
-                        request, 'Please enter valid spotify playlist')
+
+            if not URI_:
+                messages.info(request, 'Please enter playlist URI')
                 return render(request, 'spotifymusicgame/createroom.html')
 
             Max_player = request.POST['Max_player']
